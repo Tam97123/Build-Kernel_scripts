1cade950f5bbf91f5ea76d3a4ffea2e567f6e789
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-07-12 15:49:09 +0700

    perf(get clang): Automatic

--- get_clang.sh
+++ get_clang.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
-CLANG_NAME=$1
+CLANG_NAME=$(grep -oE 'clang-r[0-9]+[a-z]*' "$KERNEL_DIR/build.config.common" | head -n 1)
 
 if [ -z "$CLANG_NAME" ]; then
-    echo "Error: Clang version name is blank."
-    exit 1
+ echo "Error: Invaid Clang "$CLANG_NAME"."
+ exit 1
+elif ! git clone --filter=blob:none --no-checkout https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 aosp_clang_meta; then
+ echo "Error: Can not fetch AOSP history from Google."
+ exit 1
 fi
+cd aosp_clang
 
-echo ">> Target Clang: $CLANG_NAME"
-echo ">> Fetching AOSP toolchain history graph..."
-git clone --filter=blob:none --no-checkout https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 aosp_clang_meta
+CLANG_TARGET=$(git log -n 1 --all --diff-filter=AM --format="%H" -- "$CLANG_NAME")
 
-cd aosp_clang_meta || exit 1
-
-TARGET_COMMIT=$(git log -n 1 --all --diff-filter=AM --format="%H" -- "$CLANG_NAME")
-
-if [ -z "$TARGET_COMMIT" ]; then
+if [ -z "$CLANG_TARGET" ]; then
     echo "Error: '$CLANG_NAME' not found in AOSP history."
-    cd .. && rm -rf aosp_clang_meta
+    cd .. && rm -rf aosp_clang
     exit 1
 fi
 
 git sparse-checkout init --cone
 git sparse-checkout set "$CLANG_NAME"
+git checkout "$CLANG_TARGET"
 
-echo ">> Downloading toolchain binaries..."
-git checkout "$TARGET_COMMIT"
-
-mv "$CLANG_NAME" ../toolchain/"$CLANG_NAME"
+mv "$CLANG_NAME" "$CLANG_DIR"
 cd .. && rm -rf aosp_clang_meta
