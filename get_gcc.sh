207d7bc7ab8e648c0d80af89474d5c784949c4ac
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-07-12 16:35:52 +0700

    perf(get gcc): Improve check exit code

--- get_gcc.sh
+++ get_gcc.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-echo ">> Downloading toolchain binaries..."
-git clone https://github.com/JackA1ltman/Google-GCC-Android-4.9 -b aarch64 $TOOLCHAIN/gcc/aarch64
-git clone https://github.com/JackA1ltman/Google-GCC-Android-4.9 -b arm324 $TOOLCHAIN/gcc/arm32
+if ! git clone https://github.com/JackA1ltman/Google-GCC-Android-4.9 -b aarch64 "$GCC_DIR/aarch64"; then
+ echo "Error: Can not download gcc64! Exiting..."
+ exit 1
+elif ! git clone https://github.com/JackA1ltman/Google-GCC-Android-4.9 -b arm32 "$GCC_DIR/arm32"; then
+ echo "Error: Can not download gcc32! Exiting..."
+ exit 1
+fi
