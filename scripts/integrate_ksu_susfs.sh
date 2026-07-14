80dbf4ce7c145f368fe6fd27dede3f7554c5d3d1
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-07-14 11:47:42 +0700

    fix(integrate ksu susfs): Fix wrong variable and exit when patch

--- scripts/integrate_ksu_susfs.sh
+++ scripts/integrate_ksu_susfs.sh
@@ -22,16 +22,16 @@ if [[ "$VERSION" -lt "5" || ( "$VERSION" -eq "5" && "$PATCH_LEVEL" -eq "4" ) ]];
    echo "Error: Can not download script."
    exit 1
   fi
-  chmod +x && ./susfs_inline_hook_patches.sh
+  chmod +x susfs_inline_hook_patches.sh && ./susfs_inline_hook_patches.sh
  fi
 
- if [ ! -f "$KERNEL_DIR/susfs_patch_to_$VERSION.$PATCH_VERSION.patch" ]; then
+ if [ ! -f "$KERNEL_DIR/susfs_patch_to_$VERSION.$PATCH_LEVEL.patch" ]; then
   echo "Downloading patch..."
-  if ! curl -LO https://raw.githubusercontent.com/JackA1ltman/NonGKI_Kernel_Build_2nd/refs/heads/mainline/Patches/Patch/susfs_patch_to_$VERSION.$PATCH_VERSION.patch; then
+  if ! curl -LO https://raw.githubusercontent.com/JackA1ltman/NonGKI_Kernel_Build_2nd/refs/heads/mainline/Patches/Patch/susfs_patch_to_$VERSION.$PATCH_LEVEL.patch; then
    echo "Error: Can not download patch"
    exit 1
   fi
-  patch -p1 < "susfs_patch_to_$VERSION.$PATCH_LEVEL.patch"
+  patch -p1 < "susfs_patch_to_$VERSION.$PATCH_LEVEL.patch" || true
  fi
 else
  echo "Temporary not support GKI kernel. Aborting..."
@@ -46,8 +46,7 @@ if [ ${#REJ_FILES[@]} -gt 0 ]; then
  read -t 10 -p "Continue build kernel? (y/N): " COLLECT_REJECTS
  while true; do
   if [ -z "$COLLECT_REJECTS" ]; then
-   echo ""
-   echo "[+] Collecting .rej and .orig files into $REJECT_DIR and continue"
+   echo -e "\n[+] Collecting .rej and .orig files into $REJECT_DIR and continue"
    mkdir -p "$REJECT_DIR"
    find . -type f \( -name "*.rej" -o -name "*.orig" \) -exec mv {} "$REJECT_DIR/" \;
    break
