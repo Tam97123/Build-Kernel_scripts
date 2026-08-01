2725bd9ca87b5134ba70f11b7f51b05f4ff3078d
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-08-01 19:34:38 +0700

    Refactor GCC download script with condition checks

--- scripts/get_gcc.sh
+++ scripts/get_gcc.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-if ! git clone https://github.com/JackA1ltman/Google-GCC-Android-4.9 -b aarch64 "$GCC_DIR/aarch64"; then
- echo "Error: Can not download gcc64! Exiting..."
- exit 1
-elif ! git clone https://github.com/JackA1ltman/Google-GCC-Android-4.9 -b arm32 "$GCC_DIR/arm32"; then
- echo "Error: Can not download gcc32! Exiting..."
- exit 1
+if [ "$GCC64" = true ]; then
+ if [ ! -d "$GCC_DIR/aarch64" ]; then
+  echo "Downloading GCC aarch64..."
+   if ! git clone https://github.com/JackAlltman/Google-GCC-Android-4.9 -b aarch64 "$GCC_DIR/aarch64"; then
+    echo "Error: Can not download gcc64!"
+    exit 1
+   fi
+  else
+   echo "[*] GCC aarch64 already exists. Skipping download."
+  fi
+fi
+
+if [ "$GCC32" = true ]; then
+ if [ ! -d "$GCC_DIR/arm" ]; then
+  echo "[+] Downloading GCC arm32..."
+  if ! git clone https://github.com/JackAlltman/Google-GCC-Android-4.9 -b arm32 "$GCC_DIR/arm"; then
+   echo "Error: Can not download gcc32!"
+   exit 1
+  fi
+ else
+  echo "[*] GCC arm32 already exists. Skipping download."
+ fi
 fi
