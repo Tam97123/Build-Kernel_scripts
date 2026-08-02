86586bc56c3078dab6d11bc21adc0334863b4d39
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-08-02 11:08:27 +0700

    Update integrate_ksu.sh

--- scripts/integrate_ksu.sh
+++ scripts/integrate_ksu.sh
@@ -2,9 +2,9 @@
 
 # Integrate KernelSU (ReSukiSU)
 if [ ! -d "$KERNEL_DIR/KernelSU" ]; then
- echo "Downloading KernelSU..."
+ echo "[+] Downloading KernelSU..."
  if ! curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash; then
-  echo "Error: Can not download KernelSU"
+  echo "[-] Error: Can not download KernelSU" >&2
   exit 1
  fi
 elif [ -f "$KERNEL_DIR/KernelSU/kernel/Kbuild" ]; then
@@ -14,9 +14,9 @@ fi
 
 # Patch KernelSU manual hook
 if [ ! -f "$KERNEL_DIR/syscall_hook_patches.sh" ]; then
- echo "Downloading script..."
+ echo "[+] Downloading script..."
  if ! curl -LO https://raw.githubusercontent.com/JackA1ltman/NonGKI_Kernel_Build_2nd/refs/heads/mainline/Patches/syscall_hook_patches.sh; then
-  echo "Error: Can not download script."
+  echo "[-] Error: Can not download script." >&2
   exit 1
  fi
  chmod +x syscall_hook_patches.sh && bash syscall_hook_patches.sh
