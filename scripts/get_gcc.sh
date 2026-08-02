b1229673a89ba1a60f9766da5aa5c6d1b1e14bb1
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-08-02 10:47:13 +0700

    Update get_gcc.sh

--- scripts/get_gcc.sh
+++ scripts/get_gcc.sh
@@ -4,7 +4,7 @@ if [ "$GCC64" = true ]; then
  if [ ! -d "$GCC_DIR/aarch64" ]; then
   echo "Downloading GCC aarch64..."
    if ! git clone https://github.com/JackAlltman/Google-GCC-Android-4.9 -b aarch64 "$GCC_DIR/aarch64"; then
-    echo "Error: Can not download gcc64!"
+    echo "Error: Can not download gcc64!" >&2
     exit 1
    fi
   else
@@ -16,7 +16,7 @@ if [ "$GCC32" = true ]; then
  if [ ! -d "$GCC_DIR/arm" ]; then
   echo "[+] Downloading GCC arm32..."
   if ! git clone https://github.com/JackAlltman/Google-GCC-Android-4.9 -b arm32 "$GCC_DIR/arm"; then
-   echo "Error: Can not download gcc32!"
+   echo "Error: Can not download gcc32!" >&2
    exit 1
   fi
  else
