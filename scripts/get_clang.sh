6ff44a263da7b2ac7d57fa70cd4dfe1d03355467
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-08-02 10:45:52 +0700

    Update get_clang.sh

--- scripts/get_clang.sh
+++ scripts/get_clang.sh
@@ -3,10 +3,10 @@
 CLANG_NAME="${CLANG_NAME:-$(grep -hoE 'clang-r[0-9]+[a-z]*' "$KERNEL_DIR"/build.config.* 2>/dev/null | head -n 1)}"
 
 if [ -z "$CLANG_NAME" ]; then
- echo "Error: Can not identify clang name."
+ echo "Error: Can not identify clang name." >&2
  exit 1
 elif ! git clone --filter=blob:none --no-checkout https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 aosp_clang; then
- echo "Error: Can not fetch AOSP history from Google."
+ echo "Error: Can not fetch AOSP history from Google." >&2
  exit 1
 fi
 cd aosp_clang
@@ -14,7 +14,7 @@ cd aosp_clang
 CLANG_TARGET=$(git log -n 1 --all --diff-filter=AM --format="%H" -- "$CLANG_NAME")
 
 if [ -z "$CLANG_TARGET" ]; then
- echo "Error: '$CLANG_NAME' not found in AOSP history."
+ echo "Error: '$CLANG_NAME' not found in AOSP history." >&2
  cd .. && rm -rf aosp_clang
  exit 1
 fi
