92241c59fe14951cf36a7e2f6759a8f83b823882
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-08-10 12:29:33 +0700

    Rework check get gcc

--- scripts/get_gcc.sh
+++ scripts/get_gcc.sh
@@ -2,26 +2,19 @@
 
 mkdir -p "$GCC_DIR"
 
-if [ "$GCC64" = true ]; then
- if [ ! -d "$GCC_DIR/aarch64" ]; then
-  echo "[+] Downloading GCC aarch64..."
-   if ! git clone https://github.com/Tam97123/Google-GCC-Android -b aarch64 "$GCC_DIR/aarch64"; then
-    echo "[-] Error: Can not download gcc64!" >&2
-    exit 1
-   fi
-  else
-   echo "[*] GCC aarch64 already exists. Skipping download."
-  fi
-fi
-
-if [ "$GCC32" = true ]; then
- if [ ! -d "$GCC_DIR/arm" ]; then
-  echo "[+] Downloading GCC arm32..."
-  if ! git clone https://github.com/Tam97123/Google-GCC-Android -b arm32 "$GCC_DIR/arm"; then
-   echo "[-] Error: Can not download gcc32!" >&2
+if [[ "$GCC64" = true && ! -d "$GCC_DIR/aarch64" ]]; then
+ echo "[+] Downloading GCC aarch64..."
+  if ! git clone https://github.com/Tam97123/Google-GCC-Android -b aarch64 "$GCC_DIR/aarch64" >/dev/null; then
+   echo "[-] Error: Can not download gcc64!" >&2
    exit 1
   fi
- else
-  echo "[*] GCC arm32 already exists. Skipping download."
+ fi
+fi
+
+if [[ "$GCC32" = true && ! -d "$GCC_DIR/arm" ]]; then
+ echo "[+] Downloading GCC arm32..."
+ if ! git clone https://github.com/Tam97123/Google-GCC-Android -b arm32 "$GCC_DIR/arm" >/dev/null; then
+  echo "[-] Error: Can not download gcc32!" >&2
+  exit 1
  fi
 fi
